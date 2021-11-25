30b4550a5d910bef40f227edbc905734ac6ccdb6
petrykinaa <base151@rambler.ru>
2021-11-25 16:10:13 +0300

    added product-page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,27 @@
 $(function () {
+ 
+
+  $('.product__num').styler();
+  
+
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    draggable: false
+  });
+
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',   
+    draggable: false,
+    arrows: false,
+    fade: true
+  });
+
+
+
+
 
   $('.slider__list').slick({
 
@@ -10,5 +33,5 @@ $(function () {
 
   });
 
-  
+
 });
